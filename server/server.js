0277f7436c0e6c250796465a5516e7808d9e961d
Kelly R Cano <krcano2001@gmail.com>
2022-01-17 13:43:59 -0800

    updated the auth.js and server.js to work with apollo server and graphQl

--- server/server.js
+++ server/server.js
@@ -2,9 +2,21 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+// MY CHANGES-----------------------------
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+// uses the apollo server with typedefs and resolvers
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+// aplies middleware for apollo server
+server.applyMiddleware({ app });
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -13,9 +25,20 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+// the below commetned out code is from the restful api set up
+// app.use(routes);
 
-app.use(routes);
+// db.once('open', () => {
+//   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+// });
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    // log where we can go to test our GQL API
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });

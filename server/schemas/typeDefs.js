25953553fb88fe57c4f4e96c5531cf999cdd689a
Kelly R Cano <krcano2001@gmail.com>
2022-01-19 10:05:05 -0800

    added mutuations and queries to their files of mutaions.js and queries.js

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -5,13 +5,23 @@ const { gql } = require("apollo-server-express");
 const typeDefs = gql`
 
 
-type User:{
-    _id
+type User {
+    _id: ID!
     username: String
     email: String
     bookCount: Int
-    savedBooks:[Book]
+    savedBooks: Book
 }
+
+type Book {
+    bookId: String
+    authors:[String]
+    description: String
+    title: String
+    image: String
+    link: String
+}
+
 input BookInput{
     bookId: String
     authors:[String]
@@ -30,7 +40,7 @@ type Query{
 type Mutation{
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-    saveBook(bookId: String!, input:BookInput ): User
+    saveBook(bookId: String!, input:BookInput! ): User
     removeBook(bookId:String!): User
 
 }

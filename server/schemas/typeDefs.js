ce005ab706915783cd6f6b72b4e15db9aa1bd344
Kelly R Cano <krcano2001@gmail.com>
2022-01-21 22:20:37 -0800

    added Book as an array to typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,50 +1,43 @@
 const { gql } = require("apollo-server-express");
 
-//Did i write the authors part for an array of authors correctly
-// How do i write the savebook mutation as a input type and why am i getting the syntax error
 const typeDefs = gql`
-
-
-type User {
+  type User {
     _id: ID!
     username: String!
     email: String
     bookCount: Int
-    savedBooks: Book
-}
+    savedBooks: [Book]
+  }
 
-type Book {
+  type Book {
     bookId: ID!
-    authors:[String]
+    authors: [String]
     description: String
     title: String
     image: String
     link: String
-}
+  }
 
-input BookInput{
+  input BookInput {
     bookId: String
-    authors:[String]
+    authors: [String]
     description: String
     title: String
     image: String
     link: String
-}
-type Auth{
+  }
+  type Auth {
     token: ID!
     user: User
-}
-type Query{
+  }
+  type Query {
     me: User
-}
-type Mutation{
+  }
+  type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-    saveBook( bookData:BookInput! ): User
-    removeBook(bookId:String!): User
-
-}
-
-
+    saveBook(bookData: BookInput!): User
+    removeBook(bookId: String!): User
+  }
 `;
 module.exports = typeDefs;
